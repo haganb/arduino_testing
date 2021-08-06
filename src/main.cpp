ee8af1bf984c07f38c673a0525cd810bbd6b3787
haganb <36237020+haganb@users.noreply.github.com>
2021-08-06 18:42:40 -0400

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  Serial.write(" -- BEGIN --");
+}
+
+void toggleButton(){
+
+}
+
+void toggleBulb(){
+  
+}
+
+void loop() {
+  delay(1000);
+}
